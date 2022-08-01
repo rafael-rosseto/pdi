4db9d5c378d2dcc062224a0e9a889a7f244635ac
gabrielsmantovani <49318302+gabrielsmantovani@users.noreply.github.com>
2022-07-31 21:02:57 -0300

    Update filtraMin.cpp
    Consertando tratamento em escala de cinza.

--- filtraMin.cpp
+++ filtraMin.cpp
@@ -10,12 +10,9 @@ ImgOps imagem;
 
 int main(int argc, char *argv[]) {
     Mat input = imread(argv[1]);
+    int range = 1;
+    cvtColor(input, input, COLOR_BGR2GRAY);
     Mat output = input.clone();
-    int range = stoi(argv[2]);
-    if (input.at<Vec3b>(1, 1)[0] != input.at<Vec3b>(1, 1)[1] || input.at<Vec3b>(1, 1)[0] != input.at<Vec3b>(1, 1)[2]) {
-        cvtColor(input, output, COLOR_RGB2GRAY);
-        input = output.clone();
-    }
     imagem.filtraMin(input, output, range);
     imagem.exibir(output);
     return 0;
