a0ab38cc4d177dea157899890c0010f3eb191765
gabrielsmantovani <49318302+gabrielsmantovani@users.noreply.github.com>
2022-07-31 21:36:23 -0300

    Update otsu.cpp
    Tratamento em escala de cinza.

--- otsu.cpp
+++ otsu.cpp
@@ -10,13 +10,10 @@ ImgOps imagem;
 
 int main(int argc, char *argv[]) {
     Mat input = imread(argv[1]);
+    int lim_min = 0;
+    int lim_max = 255;
+    cvtColor(input, input, COLOR_BGR2GRAY);
     Mat output = input.clone();
-    int lim_min = stoi(argv[2]);
-    int lim_max = stoi(argv[3]);
-    if (input.at<Vec3b>(1, 1)[0] != input.at<Vec3b>(1, 1)[1] || input.at<Vec3b>(1, 1)[0] != input.at<Vec3b>(1, 1)[2]) {
-        cvtColor(input, output, COLOR_RGB2GRAY);
-        input = output.clone();
-    }
     imagem.otsu(input, output, lim_min, lim_max);
     imagem.exibir(output);
     return 0;
